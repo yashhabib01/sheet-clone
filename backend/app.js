df65836d7d06d4ab49c3e26a692a909edc946777
yash.d.habib1@gmail.com <yash.habib@lief.care>
2025-03-02 19:57:03 +0530

    fix: cors issue

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: "http://localhost:5173", // Your frontend URL
+    origin: "*", // Your frontend URL
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type"],
   })
